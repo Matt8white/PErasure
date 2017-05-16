d9716eaf0241bf457e68d265e2d1d875b642cd71
Matt8white <matt93white@gmail.com>
2017-05-16 11:27:57 +0200

    First push

---
+++ PErasure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PErasure
+//
+//  Created by Mattia Bianchi on 16/05/17.
+//  Copyright © 2017 BianchiDagrada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
