575f609daed938510d0def8ae18a017790473e61
Matt8white <matt93white@gmail.com>
2017-05-26 12:02:00 +0200

    Adjustments

--- PErasure/main.cpp
+++ PErasure/main.cpp
@@ -10,20 +10,24 @@ int main(int argc, char **argv){
 	unsigned int m, k, w, i, j, n;
 	int *matrix;
 	int *bitmatrix;
-
-	if(sscanf(argv[1], "%d", &m) == 0 || k <= 0){
+    
+    if(argc != 4) {
+        fprintf(stderr, "Please add arguments m, k, w\n");
+        exit(1);
+    }
+	if(sscanf(argv[1], "%d", &m) == 0 || m <= 0) {
 		fprintf(stderr, "Wrong m\n"); 
 		exit(1);
 	};
-	if (sscanf(argv[2], "%d", &k) == 0 || m <= 0){
+	if (sscanf(argv[2], "%d", &k) == 0 || k <= 0) {
 		fprintf(stderr, "Wrong k\n"); 
 		exit(1);
 	}; 
-	if (sscanf(argv[3], "%d", &w) == 0 || w <= 0 || w > 31){
+	if (sscanf(argv[3], "%d", &w) == 0 || w <= 0 || w > 31) {
 		fprintf(stderr, "Wrong w\n"); 
 		exit(1);
 	}; 
-	if((k + m) > (1 << w)){
+	if((k + m) > (1 << w)) {
 		fprintf(stderr, "Wrong w, the following must hold: m + k <= 2^w\n"); 
 		exit(1);
 	}; 
@@ -32,7 +36,7 @@ int main(int argc, char **argv){
 
 	for (i = 0; i < m; i++) {
 		for (j = 0; j < k; j++) {
-			n = i ^ ((1 << w)-1-j);
+			n = i ^ ((1 << w) - 1 - j);
 			matrix[i*k+j] = (n == 0) ? 0 : galois_single_divide(1, n, w);
 		}
 	}
