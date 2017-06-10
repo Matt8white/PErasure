0d9f1785c339ddda33d1b033bb6bd5b4e2163e18
Matt8white <matt93white@gmail.com>
2017-06-10 12:58:39 +0200

    Small fix

--- PErasure/run_evaluation_m8_seq.sh
+++ PErasure/run_evaluation_m8_seq.sh
@@ -1,115 +1,115 @@
 ((i = 0))
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 8 8 8 16777216 > performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 9 8 8 14913080 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 10 8 8 13421776 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 11 8 8 12201608 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 12 8 8 11184808 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 13 8 8 10324440 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 14 8 8 9586984 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 15 8 8 8947848 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 16 8 8 8388608 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 17 8 8 7895160 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 18 8 8 7456544 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 19 8 8 7064088 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 20 8 8 6710888 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 21 8 8 6391320 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 22 8 8 6100808 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 23 8 8 5835552 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 24 8 8 5592408 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 25 8 8 5368712 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 26 8 8 5162224 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 27 8 8 4971024 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 28 8 8 4793488 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 29 8 8 4628200 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 30 8 8 4473928 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 31 8 8 4329608 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 32 8 8 4194304 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 33 8 8 4067200 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 34 8 8 3947584 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 35 8 8 3834792 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 36 8 8 3728272 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 37 8 8 3627504 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 38 8 8 3532048 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 39 8 8 3441480 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 40 8 8 3355440 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 41 8 8 3273600 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 42 8 8 3195664 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 43 8 8 3121344 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 44 8 8 3050400 >> performances_m8_1gb_seq.txt
 ((i += 1))
-echo "Run number " $i " of 45"
+echo "Run number " $i " of 38"
 ./seq_main 45 8 8 2982616 >> performances_m8_1gb_seq.txt
